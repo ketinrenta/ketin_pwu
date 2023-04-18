4d2433db904b39c976e80fd75c3e764236b0eba2
ketinrenta <127809278+ketinrenta@users.noreply.github.com>
2023-04-18 12:11:36 +0800

    Add files via upload

---
+++ fromlogin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FROM  LOGIN</title>
+</head>
+<body>
+<form action="from login" method="POST">
+        <div>
+            username
+            <input type="text" name="txUSER">
+        </div>
+
+        
+        <div>
+            Password
+            <input type="text" name="txPASS">
+        </div>
+        <div>
+        <button type="submit">LOGIN</button>
+        <button type="submit">registrasi</button><br>
+        </div>
+         
+        
+</body>
+</html>
